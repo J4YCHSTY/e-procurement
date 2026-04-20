448f69013971bf483361e06ee9d46da29d35a5d2
Jay Christy <jaychristy0523@gmail.com>
2026-04-20 17:33:54 +0700

    update

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -6,15 +6,36 @@ import { Head, usePage } from "@inertiajs/react";
 
 export default function Dashboard() {
     const user = usePage().props.auth.user;
-    const [request, setRequest] = useState("");
+    const [onRequest, setRequest] = useState("");
 
     return (
         <AuthenticatedLayout header={<h2 className="font-bold text-xl text-gray-800 loading-tight">PENGAJUAN</h2>}>
             <Head title="Pengajuan" />
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <HardwareForm />
-                    <SoftwareForm />
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p6 border-l-4">
+                        <h3 className="text-lg font-bold text-gray-900 mb-4">INFORMASI PENGAJU</h3>
+                        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 text-sm bg-gray-50 p-4 rounded-md border">
+                            <div><span className="text-gray-500 block text-xs uppercase font-bold">Nama</span>{user.name}</div>
+                            <div><span className="text-gray-500 block text-xs uppercase font-bold">Email</span>{user.email}</div>
+                            <div><span className="text-gray-500 block text-xs uppercase font-bold">Perusahaan</span>{user.entity}</div>
+                            <div><span className="text-gray-500 block text-xs uppercase font-bold">Jabatan</span>{user.position}</div>
+                        </div>
+                    </div>
+                    <div className="bg-white overflow-hidden shadow-md sm:rounded-lg p-6">
+                        <label lassName="font-bold block text-sm text-gray-700 mb-2">
+                            PILIH JENIS PENGAJUAN
+                        </label>
+                        <br></br>
+                        <select className="border-gray-300 focus:border-indigo 600 focus:ring-indigo-500 rounded-sm shadow-sm w-full md:w-1/3" value={onRequest} onChange={(e) => setRequest(e.target.value)}>
+                            <option value="">-- PILIH PENGAJUAN --</option>
+                            <option value="HARDWARE">HARDWARE</option>
+                            <option value="SOFTWARE">SOFTWARE</option>
+                        </select>
+                    </div>
+
+                    {onRequest === "HARDWARE" && <HardwareForm />}
+                    {onRequest === "SOFTWARE" && <SoftwareForm />}
                 </div>
             </div>
         </AuthenticatedLayout>
--- resources/js/Pages/Software.jsx
+++ resources/js/Pages/Software.jsx
@@ -1,8 +1,26 @@
 import { useState } from 'react';
 
 const SoftwareForm = () =>  {
+    const [softwareType, setsoftwareType] = useState("");
+
     return (
-        <h1>SOFTWARE FORM</h1>
+        <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6 border-l-4 border-gray-600 mt-6 animate-fade-in-up">
+            <div>
+                <label className="block font-medium text-sm mb-2">Pilih Jenis Software Yang Dibutuhkan</label>
+                <select className="border-gray-600 focus:border-gray-500 focus:ring-gray-300 rounded-md shadow-sm w-full md:w-1/2" value={softwareType} onChange={(e) => setsoftwareType(e.target.value)}>
+                    <option value="">-- PILIH SOFTWARE --</option>
+                    <option value="Subscription">SUBSCRIPTION</option>
+                    <option value="License">LICENSE</option>
+                    <option value="Software">SOFTWARE</option>
+                </select>
+            </div>
+            <div>
+                <label className="block font-medium text-sm text-gray-600 mb-4">
+                    Alasan Pengajuan & Jelaskan Spesifikasi Khusus (Jika Ada) <span className="text-red-500">*</span>
+                </label>
+                <textarea className="border-gray-400 focus:border-gray-500 focus:ring-gray-700 rounded-md shadow-md w-full"></textarea>
+            </div>
+        </div>
     )
 }
 
