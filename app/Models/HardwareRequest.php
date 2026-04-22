60fcb2b935bba360e9094a0ba5aa38e5b20f15a6
Jay Christy <jaychristy0523@gmail.com>
2026-04-22 10:35:53 +0700

    update

--- app/Models/HardwareRequest.php
+++ app/Models/HardwareRequest.php
@@ -4,7 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class HardawreRequest extends Model
+class HardwareRequest extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'request_date',
+        'hardware_type',
+        'hardware_recommendation',
+        'justification',
+        'digital_signature',
+        'status',
+    ]
 }
--- app/Models/SoftwareRequest.php
+++ app/Models/SoftwareRequest.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SoftwareRequest extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'request_date',
+        'software_name',
+        'software_type',
+        'license_count',
+        'duration_months',
+        'estimated_cost',
+        'justification',
+        'digital_signature',
+        'status',
+    ];
 }
